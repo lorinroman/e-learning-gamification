614c9f75f9bf965e2b0439f2a239da3481da8f95
lorinroman <76521830+lorinroman@users.noreply.github.com>
2022-08-24 16:08:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./models/sequelize');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', require('./routes/index'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, async () => {
+  await sequelize.sync();
+  console.log(`Server pornit pe portul ${port}`);
+});
+
+app.use(express.static('./client/build'));
+app.get('*', (req, res) =>
+  res.sendFile('index.html', { root: './client/build' })
+);
