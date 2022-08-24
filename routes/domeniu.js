25eb42256c25d0c4ade0034ab69bd7fede94cdc8
lorinroman <76521830+lorinroman@users.noreply.github.com>
2022-08-24 16:08:06 +0300

    Add files via upload

---
+++ routes/domeniu.js
@@ -0,0 +1,25 @@
+const Domeniu = require('../models/domeniu');
+const Materie = require('../models/materie');
+
+const router = require('express').Router();
+router.get('/all', async (req, res) => {
+  try {
+    const domenii = await Domeniu.findAll();
+    res.status(200).json(domenii);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+router.get('/:id', async (req, res) => {
+  try {
+    const domeniu = await Domeniu.findByPk(req.params.id, { include: Materie });
+    if (!domeniu)
+      return res.status(404).json({ eroare: 'Nu exista acest domeniu' });
+    res.status(200).json(domeniu);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.use('/user', require('./utilizatori'));
+router.use('/domeniu', require('./domeniu'));
+router.use('/materie', require('./materie'));
+router.use('/curs', require('./curs'));
+router.use('/lectie', require('./lectie'));
+router.use('/fisier', require('./fisier'));
+router.use('/examen', require('./examen'));
+router.use('/experienta', require('./experienta'));
+router.use('/curs_student', require('./curs_student'));
+
+module.exports = router;
